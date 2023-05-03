dc2fd95c6c808d94b32f92f3271e2e740083905a
Owen Duncan-Snobel <owenduncansnobel@gmail.com>
2023-05-03 02:22:26 -0400

    Clerk Webhook setup

--- packages/server/mount/src/libs/clerk/clerk.ts
+++ packages/server/mount/src/libs/clerk/clerk.ts
@@ -3,14 +3,17 @@ import { Webhook, WebhookRequiredHeaders } from 'svix'
 import { IncomingHttpHeaders } from 'http'
 const CLERK_WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET || ''
 
+type SvixHeaders = IncomingHttpHeaders & WebhookRequiredHeaders
 
 const handleClerkWebhooks = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const payload = req.body
     const headers = req.headers;
     const wh = new Webhook(CLERK_WEBHOOK_SECRET)
-    const msg = wh.verify(payload, req.headers)
+    const msg = wh.verify(payload, headers as SvixHeaders)
+
     console.log('WEBHOOK:', msg)
+    res.status(200).json({})
   } catch (err){
     console.log(err)
     res.status(400).json({})
--- packages/server/mount/src/types/express/index.d.ts
+++ packages/server/mount/src/types/express/index.d.ts
@@ -1,15 +1,3 @@
-import { IncomingHttpHeaders } from 'http'
-import { WebhookRequiredHeaders } from 'svix'
-
-
-declare module 'http' {
-  interface IncomingHttpHeaders {
-		'svix-id': string;
-		'svix-timestamp': string;
-		'svix-signature': string;
-  }
-}
-
 declare global {
   namespace Express {
     export interface Request {
