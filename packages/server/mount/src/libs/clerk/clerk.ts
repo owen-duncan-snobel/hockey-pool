3df1f0a4f6e6f112a39a178e405a709dda84a48d
Owen Duncan-Snobel <owenduncansnobel@gmail.com>
2023-05-04 00:58:51 -0400

    ClerkWebhook "user.created" and "user.updated"

--- packages/server/mount/src/libs/clerk/clerk.ts
+++ packages/server/mount/src/libs/clerk/clerk.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from 'express'
 import { Webhook, WebhookRequiredHeaders } from 'svix'
 import { IncomingHttpHeaders } from 'http'
+import prisma from '../prisma/prisma'
+
 const CLERK_WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET || ''
 
 type SvixHeaders = IncomingHttpHeaders & WebhookRequiredHeaders
 
 interface ClerkUserCreated {
-  data: {[key: string]: any},
+  data: { [key: string]: any },
   object: string,
   type: string
 }
@@ -14,26 +16,31 @@ interface ClerkUserCreated {
 const handleClerkWebhooks = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const payload = req.body
-    const headers = req.headers;
+    const { headers } = req
     const wh = new Webhook(CLERK_WEBHOOK_SECRET)
     const msg = wh.verify(payload, headers as SvixHeaders) as ClerkUserCreated
 
-    if (msg.type === 'user.created'){
-      // insert user into db 
-      
-      // msg.data.id
-    } else if (msg.type === 'user.updated'){
-      // update any changed user properties
-
+    if (msg.type === 'user.created') {
+      await prisma.user.create({
+        data: {
+          clerk_id: msg.data.id,
+          username: msg.data.username,
+        },
+      })
+    } else if (msg.type === 'user.updated') {
+      await prisma.user.update({
+        data: {
+          username: msg.data.username,
+        },
+        where: {
+          clerk_id: msg.data.id,
+        },
+      })
     }
 
-
     res.status(200).json({})
-
-  } catch (err){
-    console.log(err)
+  } catch (err) {
     next(err)
-    res.status(400).json({})
   }
 }
 
