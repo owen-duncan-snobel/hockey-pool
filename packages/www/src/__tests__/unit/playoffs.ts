1c98fcad484ebbda479f2d7d79bc3cacf9c2e6b1
Owen Duncan-Snobel <owenduncansnobel@gmail.com>
2023-05-12 00:33:58 -0400

    updated tests

--- packages/www/src/__tests__/unit/playoffs.ts
+++ packages/www/src/__tests__/unit/playoffs.ts
@@ -11,6 +11,25 @@ describe('Playoff Utils', () => {
         expect(series).toBeDefined()
         expect(series.length).toBe(8)
       })
+
+      it('should return an empty array if no series are found', () => {
+        const series = NHLSeriesByPlayoffRound(playoff_data as unknown as IPlayoff, 5)
+        expect(series).toBeDefined()
+        expect(series.length).toBe(0)
+      })
+
+      it('should return an empty array if no rounds are found', () => {
+        const series = NHLSeriesByPlayoffRound(playoff_data as unknown as IPlayoff, 6 as unknown as 1 | 2 | 3 | 4 | 5)
+        expect(series).toBeDefined()
+        expect(series.length).toBe(0)
+      })
+
+      it('should return an empty array if no data is found', () => {
+        const series = NHLSeriesByPlayoffRound({} as unknown as IPlayoff, 1)
+        expect(series).toBeDefined()
+        expect(series.length).toBe(0)
+      })
     })
   })
+
 })
