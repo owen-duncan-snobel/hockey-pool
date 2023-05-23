eee40dc261d2b6e1b0296bb6023ec1e262aeb8bd
Owen Duncan-Snobel <owenduncansnobel@gmail.com>
2023-05-23 11:39:08 -0400

    .

--- packages/www/src/components/menu.tsx
+++ packages/www/src/components/menu.tsx
@@ -9,7 +9,8 @@ export default function MainMenu() {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-white bg-white bg-opacity-5 backdrop-blur-2xl">
+        <Menu.Button 
+          className="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-white bg-white bg-opacity-5 backdrop-blur-2xl">
           <Bars2Icon
             className="h-5 w-5 text-black"
             aria-hidden="true"
@@ -25,7 +26,8 @@ export default function MainMenu() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-2 mt-2 w-64 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items 
+          className="absolute right-2 mt-2 w-64 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="px-1 py-1 ">
             <Menu.Item>
               {({ active }) => (
